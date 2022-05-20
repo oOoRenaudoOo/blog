ee634f9b24374c22bf34998ea49c1edd721f0450
oOoRenaudoOo <salliotrenaud@gmail.com>
2022-05-20 08:49:03 -0400

    Blog Laravel - first commit - comp-tailwindcss

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence(rand(5, 10), true),
+            'content' => $this->faker->sentences(5, true),
+            'image' => 'https://via.placeholder.com/1000',
+            
+        ];
+    }
+}
