3d78723a78e90c4a1c4adc964f5e841babfd9a07
oOoRenaudoOo <salliotrenaud@gmail.com>
2022-05-24 15:11:55 -0400

    Blog-final

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -12,7 +12,7 @@
             <div class="container flex justify-between mx-auto">
                 <div class="w-full lg:w-8/12">
                     <div class="flex items-center justify-between">
-                        <h1 class="text-xl font-bold text-gray-700 md:text-2xl">Post</h1>
+                        <h1 class="text-xl font-bold text-gray-700 md:text-2xl">Liste des Posts</h1>
                         <div>
                             <select class="w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                                 <option>Latest</option>
@@ -21,59 +21,32 @@
                         </div>
                     </div>
 
-                    @foreach ($posts as $post)
+                   <?php $i = 0 ?>
+                    @forelse ($posts as $post)
                     <div class="mt-6">
                         <div class="px-10 py-6 mx-auto bg-white rounded-lg shadow-md">
-                            <div class="flex items-center justify-between">
-                                <span class="font-light text-gray-600">
-                                    {{ $post->created_at->format('d M Y') }}
-                                </span>
-                                    <a href="#"
-                                    class="px-2 py-1 font-bold text-gray-100 bg-gray-600 rounded hover:bg-gray-500">
-                                    {{ $post->category->name}}
-                                </a>
-                            </div>
-                            <div class="mt-2"><a href="#" class="text-2xl font-bold text-gray-700 hover:underline">{{ $post->title }}</a>
-                                <p class="mt-2 text-gray-600">{{ Str::limit($post->content, 120) }}</p>
-                            </div>
-                            <div class="flex items-center justify-between mt-4"><a href="{{ route('posts.show',$post) }}"
-                                    class="text-blue-500 hover:underline">Voir plus</a>
-                                <div><a href="#" class="flex items-center"><img
-                                            src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=731&amp;q=80"
-                                            alt="avatar" class="hidden object-cover w-10 h-10 mx-4 rounded-full sm:block">
-                                        <h1 class="font-bold text-gray-700 hover:underline">{{ $post->user->name }}</h1>
-                                    </a></div>
-                            </div>
+<x-PostComponent ></x-PostComponent >
+                    @empty
+                    <div class="mt-6">
+                        <div class="px-10 py-6 mx-auto bg-white rounded-lg shadow-md">
+                            <p>Il n'y a pas de post</p>
                         </div>
                     </div>
-                    @endforeach
+                    @endforelse
 
 
                     <div class="mt-8">
                         <div class="flex">
-                            <a href="#" class="px-3 py-2 mx-1 font-medium text-gray-500 bg-white rounded-md cursor-not-allowed">
-                                previous
-                            </a>
-                        
-                            <a href="#" class="px-3 py-2 mx-1 font-medium text-gray-700 bg-white rounded-md hover:bg-blue-500 hover:text-white">
-                                1
-                            </a>
-                        
-                            <a href="#" class="px-3 py-2 mx-1 font-medium text-gray-700 bg-white rounded-md hover:bg-blue-500 hover:text-white">
-                                2
-                            </a>
-                        
-                            <a href="#" class="px-3 py-2 mx-1 font-medium text-gray-700 bg-white rounded-md hover:bg-blue-500 hover:text-white">
-                                3
-                            </a>
-                        
-                            <a href="#" class="px-3 py-2 mx-1 font-medium text-gray-700 bg-white rounded-md hover:bg-blue-500 hover:text-white">
-                                Next
-                            </a>
+                            {{ $posts->onEachSide(2)->links() }}
                         </div>
                     </div>
                 </div>
-                @include('partials.sidebar')
+                <x-authorComponent :firstPost="$recent">
+                </x-authorComponent>
+
+                {{-- @include('partials.sidebar') --}}
+
+
             </div>
         </div>
         @include('partials.footer')
